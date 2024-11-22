9d3137235109af1627e1b437429b45da02d3ce0f
ttormt <ttormt@gmail.com>
2024-11-22 17:36:38 +0300

    Implement the MailHandler class

--- src/Auth/MailHandler.php
+++ src/Auth/MailHandler.php
@@ -4,20 +4,41 @@ declare(strict_types=1);
 
 namespace tTorMt\SChat\Auth;
 
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
 /**
  * Handles sending email verification and password reset links.
  */
 class MailHandler
 {
-    private string $email;
+    private PHPMailer $mailer;
+    private string $domainName;
+
     /**
      * Initializes with an email address to send links to.
      *
      * @param string $email The recipient's email address.
+     * @throws Exception
      */
     public function __construct(string $email)
     {
-        $this->email = $email;
+        $config = parse_ini_file(__DIR__.'/../../config/config.ini');
+        $this->domainName = $config['domain_name'];
+
+        $mailer = new PHPMailer(true);
+        $mailer->isSMTP();
+        $mailer->Host = $config['smtp_server'];
+        $mailer->SMTPAuth = true;
+        $mailer->Username = $config['mail_username'];
+        $mailer->Password = $config['mail_password'];
+        $mailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mailer->Port = $config['mail_port'];
+        $mailer->setFrom($config['mail_username'], 'Simple-Messenger');
+        $mailer->addAddress($email);
+        $mailer->isHTML();
+        $this->mailer = $mailer;
     }
 
     /**
@@ -25,11 +46,13 @@ class MailHandler
      *
      * @param string $linkHash
      * @return bool
+     * @throws Exception
      */
     public function sendVerificationLink(string $linkHash): bool
     {
-        // TODO: Implement sendVerificationLink() method.
-        return true;
+        $this->mailer->Subject = 'Simple-Messenger email verification';
+        $this->mailer->Body = "<h1>Email verification</h1><a href='".$this->domainName."/verifyEmail?emailVerificationToken=$linkHash"."'>Email verification link</a>";
+        return $this->mailer->send();
     }
 
     /**
@@ -37,10 +60,12 @@ class MailHandler
      *
      * @param string $linkHash
      * @return bool
+     * @throws Exception
      */
     public function sendResetPasswordLink(string $linkHash): bool
     {
-        // TODO: Implement sendResetPasswordLink() method.
-        return true;
+        $this->mailer->Subject = 'Simple-Messenger password change';
+        $this->mailer->Body = "<h1>Password change</h1><a href='".$this->domainName."/changePassword?changePassToken=$linkHash"."'>Change password link</a>";
+        return $this->mailer->send();
     }
 }
